6bf77e1663b9eebefd4e0a1f099a46002ec48881
GermanAliaga <usami.haruka01@gmail.com>
2024-11-13 19:27:58 -0300

    arreglo json

--- lib/pages/tuopinion.dart
+++ lib/pages/tuopinion.dart
@@ -21,7 +21,7 @@ class _TuopinionState extends State<Tuopinion> {
         await rootBundle.loadString("assets/tuopinion.json");
     final data = await json.decode(response);
     setState(() {
-      items = data["usabilidad"];
+      items = data["preguntas"];
     });
   }
 
@@ -35,8 +35,6 @@ class _TuopinionState extends State<Tuopinion> {
         body: Padding(
             padding: const EdgeInsets.all(25),
             child: Column(children: [
-              ElevatedButton(
-                  onPressed: readJson, child: const Text("Cargar...")),
               items.isNotEmpty
                   ? Expanded(
                       child: ListView.builder(
@@ -45,15 +43,36 @@ class _TuopinionState extends State<Tuopinion> {
                       itemBuilder: (context, index) {
                         return Card(
                             key: ValueKey(items[index]["id"]),
-                            margin: const EdgeInsets.all(10),
+                            margin: const EdgeInsets.all(5),
                             color: const Color.fromARGB(249, 232, 169, 122),
                             child: Column(children: [
-                              Text(items[index][
-                                  "titulo"]), // Asignamos el parametro ID del elemento
-                              Text(items[index][
-                                  "min"]), // Asignamos el parametro NAME del elemento
-                              Text(items[index][
-                                  "max"]), // Asignamos el parametro DESCRIPTION del elemento
+                              Text(
+                                items[index]["titulo"],
+                                style: TextStyle(fontSize: 17),
+                              ),
+                              Text(
+                                items[index]["opciones"],
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Slider(
+                                  value: 0,
+                                  max: 5,
+                                  divisions: 5,
+                                  onChanged: (double value) {}),
+                              const Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '0',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                  Text(
+                                    '5',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              )
                             ]));
                       },
                     ))
@@ -61,41 +80,3 @@ class _TuopinionState extends State<Tuopinion> {
             ])));
   }
 }
-
-/*import 'package:flutter/material.dart';
-import 'package:homecoffee/pages/mydrawer.dart';
-
-class TuOpinion extends StatefulWidget {
-  const TuOpinion({super.key});
-
-  @override
-  State<TuOpinion> createState() => _TuOpinionState();
-}
-
-class _TuOpinionState extends State<TuOpinion> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Tu opinión'),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        drawer: const MyDrawer(),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Enviar'))
-              ],
-            ),
-          ),
-        ));
-  }
-}
-*/
